0917405f1925b8ce9314c7db2b0236fbfd8dd764
Brad Howes <bradhowes@mac.com>
2022-09-22 09:13:36 +0200

    Use new TypedFullState Swift package for working with AUAudioUnit fullState values.

--- Packages/Package.swift
+++ Packages/Package.swift
@@ -6,26 +6,26 @@ let package = Package(
   name: "Packages",
   platforms: [.iOS(.v13), .macOS(.v10_15)],
   products: [
-    .library(name: "TypedFullState", targets: ["TypedFullState"]),
     .library(name: "AudioUnitHost", targets: ["AudioUnitHost"]),
     .library(name: "PresetDocumentManager", targets: ["PresetDocumentManager"])
   ],
   dependencies: [
     .package(name: "AUv3SupportPackage", url: "https://github.com/bradhowes/AUv3Support", branch: "main"),
+    .package(name: "TypedFullState", url: "https://github.com/bradhowes/typedfullstate", branch: "main"),
     .package(url: "https://github.com/apple/swift-atomics.git", .upToNextMajor(from: "1.0.0"))
   ],
   targets: [
-    .target(name: "TypedFullState"),
-    .target(
-      name: "PresetDocumentManager",
-      dependencies: [.targetItem(name: "TypedFullState", condition: nil)]
-    ),
+    .target(name: "PresetDocumentManager",
+            dependencies: [
+              .product(name: "TypedFullState", package: "TypedFullState")
+            ]
+           ),
     .target(
       name: "AudioUnitHost",
       dependencies: [
         .product(name: "AUv3-Support", package: "AUv3SupportPackage"),
-        .product(name: "Atomics", package: "swift-atomics"),
-        .targetItem(name: "TypedFullState", condition: nil)
+        .product(name: "TypedFullState", package: "TypedFullState"),
+        .product(name: "Atomics", package: "swift-atomics")
       ]
     ),
     .testTarget(
